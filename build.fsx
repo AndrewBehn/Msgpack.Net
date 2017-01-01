b9723bbce420248ec335f53cfa12f6e69c763ec5
Andrew Behn <andrew.behn@gmail.com>
2016-12-31 19:10:24 -0800

    push nuget packages to project feed in appveyor.yml

--- build.fsx
+++ build.fsx
@@ -1,6 +1,7 @@
 #r @"tools/FAKE/tools/FakeLib.dll"
 open Fake
 open Fake.DotNetCli
+open Fake.NuGetHelper
 
 let projects = (@".\src\Msgpack.Net", @".\src\UnitTests");
 let msgpackProj, unitTestProj = projects;
@@ -18,7 +19,7 @@ Target "Pack" (fun _ ->
     Pack (fun p -> { p with Project = msgpackProj;
                             Configuration = "Release";
                             }))
-    
+
 Target "Default" (fun _ ->
     trace "Building Msgpack.Net")
 
